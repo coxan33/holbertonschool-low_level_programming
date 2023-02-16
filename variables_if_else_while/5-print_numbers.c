73aac7322cbf38ef259c19a6fda1b348620d78f0
coxan33 <coe250@hotmail.com>
2023-02-16 10:07:19 -0800

    C variables_if_else_while Tasks 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+
+	return (0);
+
+}
+
