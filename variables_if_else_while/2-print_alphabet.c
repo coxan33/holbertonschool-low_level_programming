8577be69e75d722e06d44ce8f932245dfc0da59c
coxan33 <coe250@hotmail.com>
2023-02-16 07:31:17 -0800

    C variables_if_else_while Tasks 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s;
+
+	for (s = 'a' ; s <= 'z' ; s++)
+	{
+		putchar(s);
+	}	
+	{
+		putchar('\n');
+	
+	}
+		return (0);
+}
