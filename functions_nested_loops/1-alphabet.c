fb9c10590aef06cde61cce497bb409a4e67e4ec7
coxan33 <coe250@hotmail.com>
2023-02-17 06:23:30 -0800

    C functions_nested_loops Tasks 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Write a function that prints the alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
