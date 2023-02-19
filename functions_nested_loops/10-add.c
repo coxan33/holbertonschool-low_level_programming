e09e37939abd0992f8c823f3e40c01007be35076
coxan33 <coe250@hotmail.com>
2023-02-19 14:25:17 -0800

    C functions_nested_loops Tasks 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result.
+ *
+ * @n1: integer1
+ * @n2: integer2
+ *
+ * Return: 0
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
