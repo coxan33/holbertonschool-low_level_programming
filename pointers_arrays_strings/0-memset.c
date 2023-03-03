77d74154623a7d721ab3125310e990bd1072e372
coxan33 <coe250@hotmail.com>
2023-03-03 05:11:09 -0800

    C Even more pointers, arrays and strings Tasks 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset - Write a function that fills memory with a constant byte
+ *
+ * @s: Pointer
+ * @b: constant byte
+ * @n: bytes of the memory area pointed
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int cont = 0;
+	
+	for (cont = 0; cont < n; cont++)
+	{
+		s[cont] = b;
+	}
+	return (s);
+}
