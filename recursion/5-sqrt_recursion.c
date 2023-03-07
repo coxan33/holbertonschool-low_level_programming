ffb6d5012c452e65f818ae0dd6d76525912ea0e5
coxan33 <coe250@hotmail.com>
2023-03-07 09:01:53 -0800

    C Recursion Tasks 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * number - the natural square 
+ *
+ * @a: number
+ * @b: square
+ *
+ * Return: Integer
+ */
+
+int number(int a, int b)
+{
+	if (a == (b * b))
+		return (b);
+
+	else if (a > (b * b))
+		return (number(a, b + 1));
+
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ *
+ * @n: number
+ *
+ * Retrun: Integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+
+	if (n == 0)
+		return (1);
+
+	if (n < 0)
+		return (-1);
+
+	return (number(n, 1));
+}
