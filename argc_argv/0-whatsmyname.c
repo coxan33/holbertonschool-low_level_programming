e98995bb6131f326220aa08889e1438f7165fdef
coxan33 <coe250@hotmail.com>
2023-03-12 15:38:15 -0700

    C argc, argv Tasks 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ *
+ * @argc: If you rename the program, it will print the new name
+ * @argv: You should not remove the path before the name of the program
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	if (argc > 0)
+
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
