124cdc04ee103d66d6f35a0d45a31915a8d85b40
coxan33 <coe250@hotmail.com>
2023-03-14 06:02:34 -0700

    C malloc Tasks 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_array - Write a function that creates an array of chars,
+ *
+ * @size: size of array
+ * @c: Chracter
+ *
+ * Return: Char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int a = 0;
+
+	if (size > 0)
+
+		array = malloc(size * sizeof(char));
+	
+	else
+		return (NULL);
+
+	if (array == 0)
+		return (NULL);
+
+	while (a < size)
+	{
+		array[a] = c;
+		a++;
+	}
+	return (array);
+}
