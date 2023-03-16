24d79097d25113e7661fd118c2c531a40e0e1c25
coxan33 <coe250@hotmail.com>
2023-03-16 07:34:37 -0700

    C more_malloc_free Tasks 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Write a function that allocates memory using malloc
+ *
+ * @b: function should cause normal process termination with a status val of 98
+ *
+ * Return: Int
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *array;
+
+	array = malloc(b);
+
+	if (array == NULL)
+		exit (98);
+
+	else
+		return (array);
+}
