dd8c2d5e216a18ced90872f6543582d1b61970c4
coxan33 <coe250@hotmail.com>
2023-03-16 03:36:30 -0700

    C malloc Taks 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * **alloc_grid - Write a function that return a pointer to a 2 dimensional.
+ *
+ * @width: Array of integer
+ * @height: Array of integer
+ *
+ * Return: Int
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int contheight;
+	int contwidth;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int) * height);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (contheight = 0; contheight <= height; contheight++)
+	{
+		array[contheight] = malloc(sizeof(int) * width);
+		if (array[contheight] == NULL)
+		{
+			for (; contheight >= 0; contheight++)
+				free(array[contheight]);
+
+			free(array);
+			return (NULL);
+		}
+	}
+	for (contheight = 0; contheight < height; contheight++)
+	{
+		for (contwidth = 0; contwidth < width; contwidth++)
+			array[contheight][contwidth] = 0;
+	}
+	return (array);
+}
