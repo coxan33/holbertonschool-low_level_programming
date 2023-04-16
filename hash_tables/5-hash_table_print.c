916548eca520feddb84320a648695e0a80659be4
coxan33 <coe250@hotmail.com>
2023-04-16 16:01:41 -0700

    Hash_tables Tasks 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print all HT
+ *
+ * @ht: The HT
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *pointer;
+	int n;
+
+	if (ht == NULL)
+		;
+
+	else
+	{
+		printf("{");
+		for (index = 0; ht && index < ht->size; index++)
+		{
+			pointer = ht->array[index];
+			while (pointer)
+			{
+				if (n != 0)
+					printf(", ");
+				printf("'%s': '%s'", pointer->key, pointer->value);
+				pointer = pointer->next;
+				n = 1;
+			}
+		}
+		printf("}\n");
+	}
+}
